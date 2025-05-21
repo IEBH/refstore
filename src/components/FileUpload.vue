257910a3e8e04050c034d55a7b2de934af8e593f
Octian <tliang@bond.edu.au>
2025-05-21 13:20:16 +1000

    Update FileUpload.vue

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -21,14 +21,23 @@ const getReferences= () => {
 }
 </script>
 <template>
-      <div>
+      <div style="display: flex; flex-direction: column; gap: 0.5rem;">
             <tera-file-select
 			:title="'Select/Upload a file...'"
 			:filters="{library: true}"
 			placeholder="Select/Upload a file..."
 			@change="fileInputEvent($event)"
 		/>
-            <button v-if="file" @click="getReferences"></button>
+            <button v-if="file" class="btn custom-btn"  @click="getReferences">Upload</button>
       </div>
 </template>
-<style scoped></style>
+<style scoped>
+.custom-btn {
+  background-color: #579470; /* 自定义背景色 */
+  color: white;              /* 字体颜色 */
+  border: none;              /* 可选：移除边框 */
+}
+.custom-btn:hover {
+  background-color: #5da079; /* hover 效果 */
+}
+</style>
