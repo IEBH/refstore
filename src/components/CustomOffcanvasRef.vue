a3b5558fb1a1eb6eceb6a358c7a313a5af4f8974
Octian <tliang@bond.edu.au>
2025-05-22 12:06:39 +1000

    Update CustomOffcanvasRef.vue

--- src/components/CustomOffcanvasRef.vue
+++ src/components/CustomOffcanvasRef.vue
@@ -2,16 +2,17 @@
 import FileUpload from './FileUpload.vue';
 import { ref } from 'vue';
 const props = defineProps({
-      refs: Number,
-      offcanvasID: String
+      refs: Object,
+      //offcanvasID: String
 })
 const refFiles = ref(0);
-const emit = defineEmits(['updateRefs']);
+const emit = defineEmits(['updateRefsObj']);
 const getAllRefs = (e) => {
       //TODO: foreach all refs and show details + get filepath
       refFiles.value = e.length;
-      emit('updateRefs', e.length)
-      console.log("[fileRead refs]:", props.refs);
+      //emit('updateRefs', e.length)
+      emit('updateRefsObj', {id: props.refs.id, refnum: e.length})
+      console.log("[fileRead refs]:", {id: props.refs.id, refnum: e.length});
 }
 
 
@@ -20,7 +21,7 @@ const getAllRefs = (e) => {
       <div class="offcanvas offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="myOffcanvas" aria-labelledby="offcanvasLabel" style="width: 600px;">
             <nav class="navbar bg-dark border-bottom" data-bs-theme="dark">
                   <div class="container-fluid">
-                        <h5 class="navbar-brand"> {{ offcanvasID }} (References) </h5>
+                        <h5 class="navbar-brand"> {{ refs.id }} (References) </h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" />
                   </div>
             </nav>
@@ -29,7 +30,7 @@ const getAllRefs = (e) => {
                   <button type="button" class="btn-close" data-bs-dismiss="offcanvas" />
             </div>-->
             <div class="offcanvas-body">
-                  <FileUpload v-if="refs==0" @references="getAllRefs"/>
+                  <FileUpload v-if="refs.refnum==0" @references="getAllRefs"/>
                   <div class="btn-group-vertical w-100" role="group" aria-label="Vertical button group" v-if="refs>0">
                         <button class="btn glass-btn text-black" type="button">{{refFiles}}</button>
                         <button class="btn glass-btn text-black" type="button">{{refFiles}}</button>
