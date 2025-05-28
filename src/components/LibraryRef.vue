7e04934058622253b0ec57c7ebfa076e588e298b
Octian <tliang@bond.edu.au>
2025-05-28 14:42:34 +1000

    Update LibraryRef.vue

--- src/components/LibraryRef.vue
+++ src/components/LibraryRef.vue
@@ -17,7 +17,15 @@ const props =  defineProps({
                               <td class="text-start">{{ ref.title}}</td> 
                               <td class="text-start">
                                     <span v-for="author in (ref.authors || []).slice(0,3)" :key="author" class="bg-secondary text-white rounded px-2 py-1 small me-1 mb-1 d-inline-block"><i class="bi bi-person-fill"></i>{{ author }}</span>
-                                    <span v-if="(ref.authors || []).length>3" v-tooltip="{ allowHTML: true, content: ((ref.authors || []).slice(3) || []).join('<br/>') }" class="bg-body-secondary text-dark rounded px-2 py-1 small me-1 mb-1 d-inline-block"><i class="bi bi-person-plus-fill"></i> {{ref.authors.length - 3}}</span>
+                                    <span 
+                                        v-if="(ref.authors || []).length>3" 
+                                        :title="(ref.authors || []).slice(3).join('\n')"
+                                        data-bs-toggle="tooltip"
+                                        data-bs-html="true"
+                                        class="bg-body-secondary text-dark rounded px-2 py-1 small me-1 mb-1 d-inline-block"
+                                    >
+                                        <i class="bi bi-person-plus-fill"></i> {{ref.authors.length - 3}}
+                                    </span>
                               </td>
                         </tr>
                   </tbody>
