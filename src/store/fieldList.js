985c460d5e41398ec3ee112b13ee8fddc53c6b73
Octian <tliang@bond.edu.au>
2025-06-04 11:27:02 +1000

    Update fieldList.js

--- src/store/fieldList.js
+++ src/store/fieldList.js
@@ -88,23 +88,31 @@ import { vueFlowTable } from "../components/FlowTable";
   ];
 };*/
 const getDefaultFieldList = () => {
-  console.log("[defaultFieldList]", vueFlowTable.value.nodes);
+  console.log("[defaultFieldList]:", vueFlowTable.value.nodes);
   return vueFlowTable.value.nodes;
+};
+
+const getFlowTable = () => {
+  console.log("[defaultFlowTable]:", vueFlowTable.value);
+  return vueFlowTable.value;
 };
 
 const state = {
   fieldList: getDefaultFieldList(),
+  flowTable: getFlowTable(),
 };
 
 const getters = {
   getFieldList: (state) => state.fieldList,
+  getFlowTable: (state) => state.flowTable,
 };
 
 const mutations = {
   //Reset all fieldList
   reset(state) {
     state.fieldList = getDefaultFieldList();
-    console.log("[Mutations-reset]:", state.fieldList);
+    console.log("[Mutations-reset-fieldList]:", state.fieldList);
+    console.log("[Mutations-reset-flowTable]:", state.flowTable);
   },
   //Set the whole fieldlist
   setFieldList(state, fieldList) {
