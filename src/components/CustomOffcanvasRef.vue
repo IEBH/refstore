981ba684277f99c3a98ca6544c021c305f5bc1a9
Octian <tliang@bond.edu.au>
2025-06-12 15:03:31 +1000

    Update CustomOffcanvasRef.vue

--- src/components/CustomOffcanvasRef.vue
+++ src/components/CustomOffcanvasRef.vue
@@ -28,7 +28,7 @@ const getReferences= (f) => {
 const $tera = useTera();
 watch(() => props.refs.filepath, (newVal) => {
       console.log("watch-refs:", props.refs.filepath)
-      $tera.getProjectFileContents(props.refs.filepath.id).then(f=>{console.log("new:", f)})
+      $tera.getProjectFile(props.refs.filepath.id).then(f =>onsole.log("new:", f))
       if (newVal && typeof newVal.getRefs === 'function') {
             getReferences(newVal).then((refs) => {
                   console.log("watch-refs:", refs)
